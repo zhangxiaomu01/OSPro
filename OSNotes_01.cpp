4b0fbdd7b0c3d8267bdfc8e14df0304c5bf631f5
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-10-17 21:22:33 -0400

    Create OSNotes_01.cpp

---
+++ OSNotes_01.cpp
@@ -0,0 +1,88 @@
+//***************************************************************************
+//1. Main types of operating system:
+/*
+- Batch OS
+- Time sharing OS
+- Distributed OS
+- Network OS
+- Real Time OS
+- Multi-programming / processing / tasking OS
+*/
+
+
+//***************************************************************************
+//2. Functions of operating system:
+/*
+- Interface between user and hardware;
+- Allocation of resources;
+- Management of memory, security etc.
+
+The goal of OS is to make the usage of computer both convenient and efficient.
+*/
+
+
+//***************************************************************************
+//3. Important terms related to OS:
+/*
+1)Bootstrap Program: The initial program that runs when a computer is powered up
+                     or reboot.
+    - It's stored in the ROM (read-only memory)
+    - It must know how to load the OS and start executing that OS
+    - It must locate and load the OS kernel into memory
+
+2)Interrupt: The occurance of an event is typically signalled by an interrupt 
+             from a hardware or software;
+             Hardware may trigger an interrupt at anytime by sending a signal to
+             the CPU, usally by the way of system bus.
+
+3)System Call (Monitor Call): Software may trigger an interrupt by executing a 
+                              special operation called system call!
+
+
+- When a CPU is interrupted, it immediately stops what it is doing and immediately
+transfers the execution to a fixed location (usually contains the starting 
+address where the service routine of the interrupted is located);
+- The interrupt service routine executes;
+- On completion, the CPU resumes the interrupted computation.
+*/
+
+
+//***************************************************************************
+//4. I/O structore
+/*
+A general-purpose computer system consists of CPUs and multiple device controllers
+that are connected through a system bus.
+- Each device controller(maintains local buffer storage and set of special purpose
+registers) is in charge of specific type of device.
+- Typically, operating system has a device driver for each device controller. The
+driver can understand the device controller and present a uniform interface of
+the device to the rest of the operating system.
+
+Working of an I/O operation:
+1) To start the I/O operation, the device driver loads the appropriate registers
+within the device controller;
+2) The device controller, in turn, examine the contents of these registers and 
+decide which action to take;
+
+//Default transformation
+3) The controller starts the transfer of data from the device to the local buffer;
+4) Once the transfer of the data completes, the device controller informs the 
+device driver via an interrupt that it has finished the operation;
+5) The device driver then returns control to the operating system.
+
+This form of interrupt driven I/O is fine for moving small amount of data but can
+produce high overhead when used for bulk data movement. To solve this problem,
+direct memory access (DMA) is used!
+
+//With DMA, we can start from 1 - 2 - 6 - 7 - 8 here. Much faster.
+6) After setting up buffers, pointers and counters for the I/O device, the device
+controller transfers an entire block of data directly to or from its own storage
+to memory, with no intervention by the CPU.
+
+7) Only one interrupt has been generated per block, to tell the device driver 
+that the operation has been completed;
+
+8) While the device controller is performing its operations, CPU is still 
+available for other tasks.
+
+*/
